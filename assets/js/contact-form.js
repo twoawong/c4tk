a3d2fa47fb2cc678c7401a6079ef3fe79c1d2021
twoawong <wong.all@husky.neu.edu>
2014-10-06 10:50:43 -0700

    checking in images and team parts

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -6,3 +6,22 @@ $(function () {
     });
   }, 1000); // 1 second timeout protection against spam bots.
 });
+
+
+//function hello(){
+//    alert("hello");
+//}
+//
+//function compose (fn1, fn2) {
+//    return function () {
+//        fn1(fn2(arguments[0]));
+//    };
+//};
+//
+//var byename = compose(function (me) {
+//    alert("bye " + me);
+//}, function (lbah) {
+//    return lbah;
+//});
+//byename("Allen");
+//// comments
